75630945717330bccf797ec44b3545709ba43ce7
Spencer Machado <admiralmachado@gmail.com>
2012-03-28 21:41:24 -0600

    adding more fluids stuff -kevin

--- DemoDay/Fluids/TestOneDirectionFluid.h
+++ DemoDay/Fluids/TestOneDirectionFluid.h
@@ -1,15 +1,33 @@
 #include "IFluid.h"
+#include "TestOneDirectionFuildParticle.h"
 
 class TestOneDirectionFluid : public IFluid {
+private:
+
+
 public:
-<<<<<<< HEAD
-	void SetTerrain(ITerrain* newTerrain);
 
-=======
->>>>>>> dc381a2d9dd27705f824071eb1dc0a07b25e145d
-	void GetAllPoints(std::vector<cVector3d>& destination);
+	// Finds all the points in the fluid simulation and copies them into the list 'allParticles'
+	void GetAllPoints(vector<IFluidParticle*>& allParticles) {
+		//vector<TestOneDirectionFuildParticle*> allParticlesO();
+		//TestOneDirectionFuildParticle * t = new TestOneDirectionFuildParticle();
+		//allParticlesO.push_back(t);
+		//allParticles = allParticlesO;
+		
+	}
+	// Returns the number of particles in the simulation
+	int GetPointCount(void) {
+		return 1;
+	}
+	// Finds the velocity of the fluid at the given point and copies it into 'velocity'
+	void GetVelocityAt(cVector3d& velocity, const cVector3d& location) {
+	}
+	// Returns the fastest possible speed of a particle
+	double GetMaxParticleSpeed(void) {
+		return 2.0;
+	}
+	// Advances the fluid simulation one frame
+	void AdvanceFrame(void) {
+	}
 	
-	void GetVelocityAt(cVector3d& velocity, const cVector3d& location);
-
-	double GetMaxParticleSpeed(void);
 };
