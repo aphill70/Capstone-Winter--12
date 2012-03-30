80e25fde3ee48c43f7fd063d55fd07ba9813505d
Nathan Standiford <ns@byu.edu>
2012-03-30 14:24:36 -0600

    File parsing has the correct path

--- DemoDay/GEOFluid_Test/Main.cpp
+++ DemoDay/GEOFluid_Test/Main.cpp
@@ -1,6 +1,9 @@
+#include <direct.h>
+
 #include "GEOFileFluid.h"
 
 int main(void) {
-
-	GEOFileFluid fluid("brad_test_geometry", 1, 1);
+	char path[1024];
+	_getcwd(path, 1024);
+	GEOFileFluid fluid("../Fluids/fluidBake/brad_test_geometry", 1, 1);
 }
